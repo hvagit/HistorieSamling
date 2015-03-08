58ff3e9ba8b2a5c16a33cf69a682674aca810c8c
hvagit <henrik1969@gmail.com>
2015-03-08 18:05:59 +0100

    Init

--- web/js/factories.js
+++ web/js/factories.js
@@ -1,12 +1,13 @@
 var routerFactories = angular.module('routerFactories', []);
 
+/* Nedenstående er velegnet til authentication headers */
 routerFactories.factory('Filmen', ['$resource', function($resource) {
     return $resource('http://9-dot-historiesamlingservice.appspot.com/film:id', {}, 
     {
-      hentAlleFilm: {method:'GET', params:{id:''}, isArray:true},
-      opretFilm: {method:'POST'},
-      opdaterFilm: {method:'PUT', params: {id: '@id'}},
-      fjernFilm: {method:'DELETE', params: {id: '@id'}}
+      hentAlleFilm: {method:'GET', params:{id:''}, isArray:true, headers: { 'auth-token': 'C3PO R2D2' }},
+      opretFilm: {method:'POST', headers: { 'auth-token': 'C3PO R2D2' }},
+      opdaterFilm: {method:'PUT', params: {id: '@id'}, headers: { 'auth-token': 'C3PO R2D2' }},
+      fjernFilm: {method:'DELETE', params: {id: '@id'}, headers: { 'auth-token': 'C3PO R2D2' }}
     });    
 }]);
 
